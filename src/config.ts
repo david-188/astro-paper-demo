7e0209e2921095a106601d032d8368b1c50ffa25
david-188 <159600920+david-188@users.noreply.github.com>
2024-06-17 12:10:00 +0800

    修改site值

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  website: "https://soraai.me/", // replace this with your deployed domain
   author: "Sat Naing",
   desc: "An introduction and demonstration of the Sora text-to-video tool. ",
   title: "Sora Ai",
